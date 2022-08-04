7b22e67072c94f9c00bb79a80e4ca4d28cda789e
ispong <ispong@outlook.com>
2022-08-04 11:21:52 +0800

    :bug: 修复无法执行insert语法

--- star-plugin/src/main/java/com/isxcode/star/plugin/config/SparkConfig.java
+++ star-plugin/src/main/java/com/isxcode/star/plugin/config/SparkConfig.java
@@ -3,9 +3,14 @@ package com.isxcode.star.plugin.config;
 import com.isxcode.star.common.properties.StarPluginProperties;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.spark.sql.SparkSession;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.io.File;
+import java.net.URI;
+import java.net.URL;
 
 @Slf4j
 @Configuration
@@ -20,6 +25,9 @@ public class SparkConfig {
         String confPath = System.getenv("HIVE_CONF_DIR");
         String homePath = System.getenv("HIVE_HOME");
         System.out.println("打印环境变量" + confPath + ":" + homePath);
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        findConfigFile(classLoader, "hivemetastore-site.xml", false);
+        System.out.println("获取成功");
 
         log.debug("初始化sparkSession");
         SparkSession.Builder sparkBuilder = SparkSession
@@ -31,5 +39,37 @@ public class SparkConfig {
         starPluginProperties.getSparkConfig().forEach(sparkBuilder::config);
 
         return sparkBuilder.getOrCreate();
+    }
+
+    private static URL checkConfigFile(File f) {
+        try {
+            return (f.exists() && f.isFile()) ? f.toURI().toURL() : null;
+        } catch (Throwable e) {
+            System.err.println("Error looking for config " + f + ": " + e.getMessage());
+            return null;
+        }
+    }
+
+    private static URL findConfigFile(ClassLoader classLoader, String name, boolean doLog) {
+        URL result = classLoader.getResource(name);
+        if (result == null) {
+            String confPath = System.getenv("HIVE_CONF_DIR");
+            result = checkConfigFile(new File(confPath, name));
+            if (result == null) {
+                String homePath = System.getenv("HIVE_HOME");
+                String nameInConf = "conf" + File.pathSeparator + name;
+                result = checkConfigFile(new File(homePath, nameInConf));
+                if (result == null) {
+                    URI jarUri = null;
+                    try {
+                        jarUri = HiveConf.class.getProtectionDomain().getCodeSource().getLocation().toURI();
+                    } catch (Throwable e) {
+                        System.err.println("Cannot get jar URI: " + e.getMessage());
+                    }
+                    result = checkConfigFile(new File(new File(jarUri).getParentFile(), nameInConf));
+                }
+            }
+        }
+        return result;
     }
 }
