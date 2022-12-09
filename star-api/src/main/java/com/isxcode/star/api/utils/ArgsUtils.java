e392e5bf7d7088cff513c06e916798a940714f5d
ispong <ispong@outlook.com>
2022-12-09 16:07:00 +0800

    :sparkles: 修复没有依赖包的问题

--- star-api/src/main/java/com/isxcode/star/api/utils/ArgsUtils.java
+++ star-api/src/main/java/com/isxcode/star/api/utils/ArgsUtils.java
@@ -2,7 +2,9 @@ package com.isxcode.star.api.utils;
 
 import com.alibaba.fastjson2.JSON;
 import com.isxcode.star.api.pojo.StarRequest;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class ArgsUtils {
 
     public static StarRequest parse(String[] args) {
@@ -11,6 +13,7 @@ public class ArgsUtils {
             return new StarRequest();
         }
 
+        log.info("args: {}", args[0]);
         return JSON.parseObject(String.valueOf(args[0]), StarRequest.class);
     }
 }
