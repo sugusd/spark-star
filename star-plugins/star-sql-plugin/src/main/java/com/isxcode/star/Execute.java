278a0bf3bc47492a553e0de0b8b846c230b4adcc
ispong <ispong@outlook.com>
2022-12-09 12:26:28 +0800

    :sparkles: 支持spark插件化开发

--- star-plugins/star-sql-plugin/src/main/java/com/isxcode/star/Execute.java
+++ star-plugins/star-sql-plugin/src/main/java/com/isxcode/star/Execute.java
@@ -1,8 +1,13 @@
 package com.isxcode.star;
 
+import org.apache.logging.log4j.util.Strings;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Execute {
 
@@ -15,14 +20,24 @@ public class Execute {
             .enableHiveSupport()
             .getOrCreate();
 
-        Dataset<Row> rowDataset = sparkSession.sql("select * from ispong_db.users");
-
-        // 执行spark
+        Dataset<Row> rowDataset = sparkSession.sql("select * from ispong_db.users").limit(10);
+        System.out.println("rowDataset: 获取dataset");
         System.out.println("===> 返回");
 
-        rowDataset.foreach(e -> {
-            System.out.println(e.get(0));
+        String[] columns = rowDataset.columns();
+        System.out.println("columns: " + Arrays.toString(columns));
+
+        // 获取数据值
+        List<List<String>> dataList = new ArrayList<>();
+        List<Row> rows = rowDataset.collectAsList();
+        rows.forEach(e -> {
+            List<String> metaData = new ArrayList<>();
+            for (int i = 0; i < e.size(); i++) {
+                metaData.add(String.valueOf(e.get(i)));
+            }
+            dataList.add(metaData);
         });
+        System.out.println("dataList: " + dataList);
 
         // 停止session
         sparkSession.stop();
