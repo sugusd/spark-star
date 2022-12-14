20cabdd1af485aed99342266de050fc0cc64f033
ispong <ispong@outlook.com>
2022-12-14 15:43:17 +0800

    :memo: 优化文档

--- star-server/src/main/java/com/isxcode/star/server/config/StarServerConfig.java
+++ star-server/src/main/java/com/isxcode/star/server/config/StarServerConfig.java
@@ -1,18 +1,20 @@
 package com.isxcode.star.server.config;
 
-
-import com.isxcode.star.api.exception.StarException;
 import com.isxcode.star.api.properties.StarProperties;
-import com.isxcode.star.common.config.CommonAutoConfiguration;
+import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableConfigurationProperties(StarProperties.class)
 public class StarServerConfig {
+
+    private final ConfigurableApplicationContext context;
 
     @Bean
     @ConditionalOnClass(StarServerConfig.class)
@@ -20,12 +22,14 @@ public class StarServerConfig {
 
         String starHomePath = System.getenv("STAR_HOME");
         if (Strings.isEmpty(starHomePath)) {
-            throw new StarException("50010", "请配置STAR_HOME环境变量");
+            System.out.println("ERROR:请配置STAR_HOME环境变量");
+            context.close();
         }
 
         String yarnConfDir = System.getenv("YARN_CONF_DIR");
         if (Strings.isEmpty(yarnConfDir)) {
-            throw new StarException("50011", "请配置YARN_CONF_DIR环境变量");
+            System.out.println("ERROR:请配置YARN_CONF_DIR环境变量");
+            context.close();
         }
 
     }
