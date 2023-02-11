ca5f40d8237757bf1e235650241d86cbe4be0c59
ispong <ispong@outlook.com>
2023-02-11 20:15:03 +0800

    :white_check_mark: 测试真实项目

--- star-local-test/use-client-example/src/main/java/com/isxcode/star/example/controller/ExampleController.java
+++ star-local-test/use-client-example/src/main/java/com/isxcode/star/example/controller/ExampleController.java
@@ -24,10 +24,83 @@ public class ExampleController {
 
     private final StarTemplate starTemplate;
 
-    @GetMapping("/execute")
-    public StarResponse execute(@RequestParam String sql) {
+    // 内存 10核50GB
+    // 最小 1核1GB
+    // 最大 2核5GB
+    @GetMapping("/executeHive")
+    public StarResponse executeHive() {
 
-        return starTemplate.build().sql(sql).execute();
+        // 20核40GB
+        Map<String, String> sparkConfig = new HashMap<>();
+
+        // 配置spark可使用的资源  1个容器一个核  * 最大的容器内存
+        sparkConfig.put("spark.yarn.am.memoryr", "50g"); // 默认512m
+        sparkConfig.put("spark.yarn.am.cores", "10"); // 默认1
+
+        // driver 负责加载数据，需要大并发，可以不用配置 配置无效
+        // sparkConfig.put("spark.driver.memory","4g");
+        // driver的个数
+        //sparkConfig.put("spark.driver.cores","20");
+
+        // 单个执行器的配置 处理数据，处理数据10个并发进程，由于spark计算需要比实际要大，所以要比容器最大值，低一点点，必须是整数
+        // 这个值需要调大  （4g<5g 需要小于yarn的容器）
+        sparkConfig.put("spark.executor.memory", "4g");
+        // 每个执行器执行2个task (需要小于yarn的容器最大核数)
+        sparkConfig.put("spark.executor.cores", "2");
+        // 执行器的个数 （2*9=18个执行器）需要分配 9+1=10 yarn开启10个容器
+        sparkConfig.put("spark.executor.instances", "9");
+
+        sparkConfig.put("hive.metastore.uris", "thrift://localhost:9083");
+
+        String sql = "insert overwrite table dws_dfg_logsheet_report\n" +
+            "select \n" +
+            "    mw.block_id,\n" +
+            "    mw.tran_time,\n" +
+            "    mw.res_id,\n" +
+            "    mw.mat_id,\n" +
+            "    ef.recipe_id,\n" +
+            "    me.lot_id,\n" +
+            "    mw.in_mat_num,\n" +
+            "    mw.out_mat_num,\n" +
+            "    tqs.wafer_id,\n" +
+            "    tqs.center_thickness,\n" +
+            "    tqs.ttv_gbir,\n" +
+            "    me.mf1_wlm,\n" +
+            "    me.mf1_blm,\n" +
+            "    me.mf1_bsd,\n" +
+            "    me.mf2_wpc,\n" +
+            "    me.mf2_bpc,\n" +
+            "    me.mf2_bsd,\n" +
+            "    os.bad_type,\n" +
+            "    os.loss_qty,\n" +
+            "    me.usable_tooth,\n" +
+            "    me.wheel_well_ratio,\n" +
+            "    me.tatal_grainding_time,\n" +
+            "    me.warm_up_time_current,\n" +
+            "    me.z_spindle_current,\n" +
+            "    me.spindle_rpm,\n" +
+            "    me.cooling_water,\n" +
+            "    me.cutting_water,\n" +
+            "    me.safty_cutting_water,\n" +
+            "    me.ipg_cooling_water,\n" +
+            "    me.usable_tooth_2,\n" +
+            "    me.wheel_well_ratio_2,\n" +
+            "    me.tatal_grainding_time_2,\n" +
+            "    me.warm_up_time_current_2,\n" +
+            "    me.z_spindle_current_2,\n" +
+            "    me.spindle_rpm_2,\n" +
+            "    me.cooling_water_2,\n" +
+            "    me.cutting_water_2,\n" +
+            "    me.safty_cutting_water_2,\n" +
+            "    me.ipg_cooling_water_2,\n" +
+            "    mw.remark\n" +
+            "from dwd_mwiplothis mw\n" +
+            "left join dwd_MRCPMFODEF ef on ef.mat_id = mw.mat_id and ef.flow = mw.flow and ef.oper = mw.oper\n" +
+            "left join  dwd_MWIPLOTLOS os on mw.lot_id = os.lot_id \n" +
+            "left join dwd_TQS_SUMMARY_DATA tqs on mw.lot_id = tqs.wafer_id\n" +
+            "left join dwd_MEDCRESDAT me on tqs.wafer_id = me.unit_id";
+
+        return starTemplate.build().sparkConfig(sparkConfig).sql(sql).execute();
     }
 
     @GetMapping("/executeMysql")
@@ -64,6 +137,41 @@ public class ExampleController {
     @GetMapping("/executeSync")
     public StarResponse executeSync() {
 
+        // 20核40GB
+        Map<String, String> sparkConfig = new HashMap<>();
+
+        // 配置spark可使用的资源  1个容器一个核  * 最大的容器内存
+        sparkConfig.put("spark.yarn.am.memory", "50g"); // 默认512m
+        sparkConfig.put("spark.yarn.am.cores", "10"); // 默认1
+
+        // driver 负责加载数据，需要大并发，可以不用配置 配置无效
+        // sparkConfig.put("spark.driver.memory","4g");
+        // driver的个数
+        //sparkConfig.put("spark.driver.cores","20");
+
+        // 单个执行器的配置 处理数据，处理数据10个并发进程，由于spark计算需要比实际要大，所以要比容器最大值，低一点点，必须是整数
+        // 这个值需要调大  （4g<5g 需要小于yarn的容器）
+        sparkConfig.put("spark.executor.memory", "4g");
+        // 每个执行器执行2个task (需要小于yarn的容器最大核数)
+        sparkConfig.put("spark.executor.cores", "2");
+        // 执行器的个数 （2*9=18个执行器）需要分配 9+1=10 yarn开启10个容器
+        sparkConfig.put("spark.executor.instances", "1");
+
+        sparkConfig.put("hive.metastore.uris", "thrift://localhost:9083");
+
+//        String sql = "" +
+//            "CREATE TEMPORARY VIEW users_view\n" +
+//            "USING org.apache.spark.sql.jdbc\n" +
+//            "OPTIONS (\n" +
+//            "    driver 'com.mysql.cj.jdbc.Driver',\n" +
+//            "    url 'jdbc:mysql://47.92.168.116:30102/fanruan_report',\n" +
+//            "    user 'fanruan',\n" +
+//            "    password 'fanruan2023',\n" +
+//            "    dbtable 'ads_dfg_logsheet_report'\n" +
+//            ");\n" +
+//            "" +
+//            "insert into users_view select * from dws_dfg_logsheet_report";
+
         String sql = "" +
             "CREATE TEMPORARY VIEW users_view\n" +
             "USING org.apache.spark.sql.jdbc\n" +
@@ -75,9 +183,9 @@ public class ExampleController {
             "    dbtable 'users'\n" +
             ");\n" +
             "" +
-            "insert into ispong_db.users select * from users_view";
+            "insert into users_view select * from dws_dfg_logsheet_report";
 
-        return starTemplate.build().sql(sql).execute();
+        return starTemplate.build().sparkConfig(sparkConfig).sql(sql).execute();
     }
 
     @GetMapping("/getStatus")
