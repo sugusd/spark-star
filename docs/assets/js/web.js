86917edb85b9e81566661dd936a9b16c7c76a632
ispong <ispong@outlook.com>
2023-03-04 13:45:03 +0800

    :sparkles: 构建官方文档

--- docs/assets/js/web.js
+++ docs/assets/js/web.js
@@ -5,8 +5,13 @@ function show_func_link(index) {
     document.getElementById('func-' + funcIndex).style.display = "none";
     document.getElementById('show-func-link-' + funcIndex).style.borderBottom = "none";
 
-    document.getElementById('func-' + index).style.display = "block";
+    document.getElementById('func-' + index).style.display = "flex";
     document.getElementById('show-func-link-' + index).style.borderBottom = "black solid 3px";
 
     funcIndex = index;
+}
+
+function scrollToDiv(div_name) {
+    let div = document.getElementById(div_name);
+    div.scrollIntoView({ behavior: 'smooth' });
 }
