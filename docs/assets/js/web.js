4290e6949b4fbe5670a4aa7c4764562fb542da78
ispong <ispong@outlook.com>
2023-03-09 23:00:26 +0800

    :sparkles: 手机不可访问演示环境

--- docs/assets/js/web.js
+++ docs/assets/js/web.js
@@ -15,3 +15,16 @@ function scrollToDiv(div_name) {
     let div = document.getElementById(div_name);
     div.scrollIntoView({ behavior: 'smooth' });
 }
+
+function isMobileDevice() {
+    return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+}
+
+function demoBtn() {
+
+    if (isMobileDevice()) {
+        alert("请使用电脑访问！");
+    } else {
+        window.open('https://zhiqingyun-demo.isxcode.com');
+    }
+}
