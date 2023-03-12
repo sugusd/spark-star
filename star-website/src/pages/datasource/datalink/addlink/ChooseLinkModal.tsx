0c6ec0a4d1ab9e2b45dff036f7953f0b180f1ddf
ispong <ispong@outlook.com>
2023-03-12 23:31:21 +0800

    :sparkles: 添加数据源功能

--- star-website/src/pages/datasource/datalink/addlink/ChooseLinkModal.tsx
+++ star-website/src/pages/datasource/datalink/addlink/ChooseLinkModal.tsx
@@ -25,12 +25,19 @@ export const ChooseLinkModal = (
             onCancel={props.handleCancel}
             width={500}>
             <div className={"choose-link-div"}>
+                <div className={"datasource-type"} onClick={() => handleOk()}>Api</div>
+                <div className={"datasource-type"} onClick={() => handleOk()}>Clickhouse</div>
+                <div className={"datasource-type"} onClick={() => handleOk()}>Doris</div>
+                <div className={"datasource-type"} onClick={() => handleOk()}>Elastic</div>
+                <div className={"datasource-type"} onClick={() => handleOk()}>H2</div>
+                <div className={"datasource-type"} onClick={() => handleOk()}>Hbase</div>
+                <div className={"datasource-type"} onClick={() => handleOk()}>Hive</div>
                 <div className={"datasource-type"} onClick={() => handleOk()}>Mysql</div>
-                <div className={"datasource-type"}>Oracle</div>
-                <div className={"datasource-type"}>MongoDB</div>
-                <div className={"datasource-type"}>PostgreSQL</div>
-                <div className={"datasource-type"}>H2</div>
-                <div className={"datasource-type"}>Hive</div>
+                <div className={"datasource-type"} onClick={() => handleOk()}>MongoDB</div>
+                <div className={"datasource-type"} onClick={() => handleOk()}>Oracle</div>
+                <div className={"datasource-type"} onClick={() => handleOk()}>OSS</div>
+                <div className={"datasource-type"} onClick={() => handleOk()}>PostgreSQL</div>
+                <div className={"datasource-type"} onClick={() => handleOk()}>SqlServer</div>
             </div>
         </Modal>
         <AddLinkModal handleCancel={handleCancel} handleOk={handleOk} isModalVisible={isModalVisible}/>
